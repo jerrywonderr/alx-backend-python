19d58473ca62d809f9cf975a80391ee188e7fcd4
Jerry Wonder <jerrycul2001@gmail.com>
2023-07-06 09:44:03 +0100

    feat: complete simple add function with annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A simple function that adds two numbers and returns their sum"""
+
+
+def add(a: float, b: float) -> float:
+    """Adds two numbers, returns their sum"""
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,3 @@
+# Annotations in Python
+This folder contains my learnings on annotations in python and how to use the
+typings module for type hints and type checks for varaibles and functions.
